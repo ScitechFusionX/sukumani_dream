494b00f3a81208574668edb695956af4c28d2d8e
thandomy <1132720@students.wits.ac.za>
2017-08-05 12:28:25 +0200

    Added the website in angular

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component }          from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Children</a>
+      <a routerLink="/heroes" routerLinkActive="active">Therapists</a>
+      <a routerLink="/heroes" routerLinkActive="active">Stands</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Sukumani Dream';
+}
---
+++ my-app/src/app/hero.ts
@@ -0,0 +1,11 @@
+export class Hero {
+  id: number;
+  name: string;
+  dob:string;
+  gender:string;
+  diagnosis:string;
+  gmfcs_level:string;
+  caregiver_name:string;
+  contact:string;
+  residential_area:string;
+}
